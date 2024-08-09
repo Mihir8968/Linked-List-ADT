a229f97af6e3b538cdc1ddce98e9e74c221eb18b
Mihir Anand <148095211+Mihir8968@users.noreply.github.com>
2024-08-09 20:16:41 +0530

    bug fix

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
-### test.cpp
-
-```cpp
+#include <bits/stdc++.h>
+using namespace std;
 #include "Linked_List_ADT.h"
 
-int main() {
+using namespace std;
+
+int main()
+{
     LinkedList<int> list;
 
     // Test insertion
@@ -11,39 +13,39 @@ int main() {
     list.insert(3, 1);
     list.insert(2, 1); // Inserting 2 at position 1
     list.insert(4, 3); // Inserting 4 at the end
-    list.display();    // Expected Output: 1 2 3 4 
+    list.display();    // Expected Output: 1 2 3 4
 
     // Test deletion
-    list.deleteByValue(3); 
-    list.display();    // Expected Output: 1 2 4
+    list.deleteByValue(3);
+    list.display(); // Expected Output: 1 2 4
 
     // Test reverse
     list.reverse();
-    list.display();    // Expected Output: 4 2 1
+    list.display(); // Expected Output: 4 2 1
 
     // Test length
     cout << "Length of list: " << list.length() << endl; // Expected Output: 3
 
     // Test search
-    Node* found = list.find(2);
+    Node<int> *found = list.find(2);
     cout << "Found value: " << (found ? found->data : -1) << endl; // Expected Output: 2
 
     // Test concatenation
-    LinkedList list2;
+    LinkedList<int> list2;
     list2.insert(5, 0);
     list2.insert(6, 1);
     list2.insert(7, 2);
     list.concatenate(list2);
-    list.display();    // Expected Output: 4 2 1 5 6 7
+    list.display(); // Expected Output: 4 2 1 5 6 7
 
     // Test merge
-    LinkedList list3;
+    LinkedList<int> list3;
     list3.insert(8, 0);
     list3.insert(3, 0); // Sorted insertions
     list3.insert(9, 2);
     list3.insert(7, 2);
     list.merge(list3);
-    list.display();    // Expected Output: 3 4 2 1 5 6 7 7 8 9
+    list.display(); // Expected Output: 3 4 2 1 5 6 7 7 8 9
 
     // Test loop detection and creation
     list.createLoopByValue(6);
